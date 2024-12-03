fb0e488f089e057b8f77eb8ff7532d7467dd913b
ChaimEzra <chimas1357@gmail.com>
2024-12-03 15:05:17 +0200

    commit by chaim

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,9 @@ import Form from "./Form";
 import PackingList from "./PackingList";
 // import Item from "./Item";
 import Stats from "./Stats";
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
 
 export default function App() {
-  const [items, setItems] = useState(initialItems);
+  const [items, setItems] = useState([]);
   function handelAddItems(item) {
     setItems((items) => [...items, item]);
   }
