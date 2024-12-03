7d4be08d21a57991c324d3adc5fb7a5d87f57f10
ChaimEzra <chimas1357@gmail.com>
2024-12-03 14:59:33 +0200

    co 5

--- src/components/Form.js
+++ src/components/Form.js
@@ -38,6 +38,7 @@ export default function Form({ onAddItems }) {
         onChange={(e) => setDescription(e.target.value)}
       />
       <button>Add</button>
+      <button>Add</button>
     </form>
   );
 }
